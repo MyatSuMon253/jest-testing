b70e3673d79577977c08d4fcf2fde54e744a0e02
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-04-19 19:13:46 +0630

    feat: jest helper to test navigation

---
+++ src/testHelpers.tsx
@@ -0,0 +1,34 @@
+import { createMemoryHistory, MemoryHistory } from "history";
+import { RenderResult, render } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import { ReactNode } from "react";
+
+type RenderWithRouter = (
+  renderComponent: () => ReactNode,
+  route?: string
+) => RenderResult & { history: MemoryHistory };
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      renderWithRouter: RenderWithRouter;
+    }
+  }
+
+  namespace globalThis {
+    const renderWithRouter: RenderWithRouter;
+  }
+}
+
+renderWithRouter(() => <div>some component</div>, "route");
+
+global.renderWithRouter = (renderComponent, route) => {
+  const history = createMemoryHistory();
+  if (route) {
+    history.push(route);
+  }
+  return {
+    ...render(<Router history={history}>{renderComponent}</Router>),
+    history,
+  };
+};
